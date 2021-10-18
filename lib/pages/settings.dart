42c65a053707ffb3929cedd8db6dae301d8f972e
santamayengb <19mca8232@cuchd.in>
2021-10-18 14:36:25 +0530

    6 error

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 class SettingPage extends StatelessWidget {
   const SettingPage({
     Key? key,
-    required this.name,
+    required this.userName,
   }) : super(key: key);
 
-  final String name;
+  final String userName;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
-          children: [Text(name)],
+          children: [Text(userName)],
         ),
       ),
     );
